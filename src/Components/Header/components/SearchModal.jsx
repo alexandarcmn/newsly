b796460d29c6c6f1ab28c1748b350b23f6895e5d
Aleksandar Milakovic <alexmilakovic24@gmail.com>
2023-01-27 16:22:40 +0100

    search feed

--- src/Components/Header/components/SearchModal.jsx
+++ src/Components/Header/components/SearchModal.jsx
@@ -3,6 +3,7 @@ import { GrClose } from "react-icons/gr";
 
 const SearchModal = ({ searchShow, setSearchShow }) => {
   const [search, setSearch] = useState("");
+
   const submitHandler = (e) => {
     e.preventDefault();
     setSearch("");
@@ -14,7 +15,7 @@ const SearchModal = ({ searchShow, setSearchShow }) => {
         <div className="row">
           <div className="col-sm-12 col-md-8 text-center m-auto">
             <div className="v1search_form">
-              <form onSubmit={submitHandler}>
+              <form method="GET" action={`https://srch.adsearchexperts.com/f?cid=4969&q=${search}`}>
                 <input
                   value={search}
                   onChange={(e) => setSearch(e.target.value)}
